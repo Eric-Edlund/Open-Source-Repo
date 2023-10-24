2885e8fc89ab480ef18f19b01792cd72c4240bc1
jwmahone <123087234+jwmahone@users.noreply.github.com>
2023-10-24 13:49:01 -0400

    added bugs

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -1,9 +1,11 @@
+//bugs have been introduced jwmahone
+
 public class sum_even_fibonacci {
     public static void main(String[] args) {
         int a = 0, b = 1;
         int sumEven = 0;
         while (b < 4000000) {
-            if (b % 2 == 0) {
+            if (b % 3 == 0) {
                 sumEven += b;
             }
             int temp = b;
