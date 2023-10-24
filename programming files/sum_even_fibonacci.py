8828993496f086eebab124e2e68e8048a71593b6
dfisher5 <122995380+dfisher5@users.noreply.github.com>
2023-10-24 14:16:22 -0400

    Fixed bug - set sum_even to 0

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -2,7 +2,7 @@
 // introduced bugs jwmahone 
 
 a, b = 0, 1 // variable a is set to 0 and variable b is set to 1
-sum_even = 4000000
+sum_even = 0
 while b < 4000000:
     if b % 2 == 0:    // If b is even,
         sum_even += b // add b to sum_even
