a6fd3e1e4270f32a8ae8303d47433f6eb362e1c3
Connor Brown <cbrown54@uvm.edu>
2023-10-24 13:38:28 -0400

    Commented sum_even_fibonacci.java

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -1,15 +1,19 @@
+// Commented by Connor Brown on 10/24/23
+
 public class sum_even_fibonacci {
     public static void main(String[] args) {
-        int a = 0, b = 1;
+        int a = 0, b = 1; // Set integer a to 0 and b to 1
         int sumEven = 0;
         while (b < 4000000) {
-            if (b % 2 == 0) {
-                sumEven += b;
+            if (b % 2 == 0) { // If b is even
+                sumEven += b; // Add b to sumEven
             }
+            // Add b's previous value to b
             int temp = b;
             b = a + b;
             a = temp;
         }
+        // Print that weird number
         System.out.println(sumEven);
     }
 }
