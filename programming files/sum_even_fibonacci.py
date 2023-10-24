bec0c15c85e127a084d238471516beed727592ba
jwmahone <123087234+jwmahone@users.noreply.github.com>
2023-10-24 13:52:49 -0400

    added bugs to sum_even_fibonacci.py

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,7 +1,8 @@
 // Commented by Connor Brown on 10/24/23
+// introduced bugs jwmahone 
 
 a, b = 0, 1 // variable a is set to 0 and variable b is set to 1
-sum_even = 0
+sum_even = 4000000
 while b < 4000000:
     if b % 2 == 0:    // If b is even,
         sum_even += b // add b to sum_even
