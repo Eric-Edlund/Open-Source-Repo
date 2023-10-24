2e022d79f2a084e1c7ec872b33b8c9d55953e285
sizsak <123182067+sizsak@users.noreply.github.com>
2023-10-24 14:16:26 -0400

    Update even_odd_site.py
    I think I fixed issue

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -5,8 +5,8 @@
 with open("numbers.html", "w") as f:
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
-    while i <= 50:
-        if i % 2 != 0:
+    for i in range(50):
+        if i % 2 == 0:
             f.write("<tr><td>{}</td><td></td></tr>\n".format(i))
         else:
             f.write("<tr><td></td><td>{}</td></tr>\n".format(i))
