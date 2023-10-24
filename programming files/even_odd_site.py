4d4a80a759c9c4cd39aa2ce1c7e425ba5b3afbe0
Connor Brown <cbrown54@uvm.edu>
2023-10-24 13:52:03 -0400

    Update even_odd_site.py

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -1,8 +1,12 @@
+// Bugged by Connor Brown on 10/24/23
+// Changed for in range loop to while loop on line 7
+// Changed if statment from == to != on line 8
+
 with open("numbers.html", "w") as f:
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
-    for i in range(1, 50):
-        if i % 2 == 0:
+    while i <= 50:
+        if i % 2 != 0:
             f.write("<tr><td>{}</td><td></td></tr>\n".format(i))
         else:
             f.write("<tr><td></td><td>{}</td></tr>\n".format(i))
